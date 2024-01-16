be0e9e0114937abd2ded54d7ad61b55a3edf3bc9
kevin <kevincolmenaresm@gmail.com>
2024-01-16 19:00:42 -0400

    Commented out unused code in 10-abstract.ts

--- src/10-abstract.ts
+++ src/10-abstract.ts
@@ -1,7 +1,7 @@
 import { Animal, Dog } from './07-protected';
 
-const animal = new Animal("Fifi", 2, 5);
-animal.greeting();
+//const animal = new Animal("Fifi", 2, 5);
+//animal.greeting();
 
 const cheis = new Dog("Cheis", 3, 10);
 
